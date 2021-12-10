e2689d941c3d5079eea07c3efcba7b3ab1c998f4
suhj622 <haojiesu@126.com>
2021-12-10 09:56:15 +0800

    用Lambda表达式使用比较器

--- src/com/suhj/strategy/SortTest.java
+++ src/com/suhj/strategy/SortTest.java
@@ -8,27 +8,32 @@ import java.util.Arrays;
 /**
  * @author Haojie
  *
- * 排序器测试类
+ * 排序器测试类-使用Lambda表达式
  */
 public class SortTest {
 	public static void main(String[] args) {
 		
 		Cat[] cats = {new Cat(2,4),new Cat(1,1),new Cat(3,1)};
-		Dog[] dogs = {new Dog(3), new Dog(2), new Dog(8)};
-		Comparator<Cat> Comparator1 = new CatHeightComparator();
-		Comparator<Cat> Comparator2 = new CatWeightComparator();
-		Comparator<Dog> Comparator3 = new DogFoodAmountComparator();
+		//Dog[] dogs = {new Dog(3), new Dog(2), new Dog(8)};
+		//Comparator<Cat> Comparator1 = new CatHeightComparator();
+		//Comparator<Cat> Comparator2 = new CatWeightComparator();
+		//Comparator<Dog> Comparator3 = new DogFoodAmountComparator();
 		
 		//用比较器排序
 		Sorter<Cat> catSorter = new Sorter<Cat>();
-		Sorter<Dog> dogSorter = new Sorter<Dog>();
+		//Sorter<Dog> dogSorter = new Sorter<Dog>();
 		
-		catSorter.sort(cats, Comparator1);
+		//高的排前面
+		catSorter.sort(cats, (o1,o2)->{
+			if(o1.getHeight() < o2.getHeight()) return 1;
+			else if (o1.getHeight() > o2.getHeight()) return -1;
+			else return 0;
+		});
 		System.out.println(Arrays.toString(cats));
-		catSorter.sort(cats, Comparator2);
-		System.out.println(Arrays.toString(cats));
-		dogSorter.sort(dogs, Comparator3);
-		System.out.println(Arrays.toString(dogs));
+		//catSorter.sort(cats, Comparator2);
+		//System.out.println(Arrays.toString(cats));
+		//dogSorter.sort(dogs, Comparator3);
+		//System.out.println(Arrays.toString(dogs));
 	}
 
 }
