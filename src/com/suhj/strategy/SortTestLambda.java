a19608d510830d3df289bb9d985708eaf02a0238
suhj622 <haojiesu@126.com>
2021-12-10 09:56:39 +0800

    用Lambda表达式使用比较器

---
+++ src/com/suhj/strategy/SortTestLambda.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.suhj.strategy;
+
+import java.util.Arrays;
+
+/**
+ * @author Haojie
+ *
+ * ≈≈–Ú∆˜≤‚ ‘¿‡
+ */
+public class SortTestLambda {
+	public static void main(String[] args) {
+		
+		Cat[] cats = {new Cat(2,4),new Cat(1,1),new Cat(3,1)};
+		Dog[] dogs = {new Dog(3), new Dog(2), new Dog(8)};
+		Comparator<Cat> Comparator1 = new CatHeightComparator();
+		Comparator<Cat> Comparator2 = new CatWeightComparator();
+		Comparator<Dog> Comparator3 = new DogFoodAmountComparator();
+		
+		//”√±»Ωœ∆˜≈≈–Ú
+		Sorter<Cat> catSorter = new Sorter<Cat>();
+		Sorter<Dog> dogSorter = new Sorter<Dog>();
+		
+		catSorter.sort(cats, Comparator1);
+		System.out.println(Arrays.toString(cats));
+		catSorter.sort(cats, Comparator2);
+		System.out.println(Arrays.toString(cats));
+		dogSorter.sort(dogs, Comparator3);
+		System.out.println(Arrays.toString(dogs));
+	}
+
+}
