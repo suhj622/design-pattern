318145170410db05080bbf29652f173a03451e2c
suhj622 <haojiesu@126.com>
2022-01-13 15:16:00 +0800

    观察者模式v1：披着面向对象外衣的面向过程

---
+++ src/com/suhj/observer/v1/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.suhj.observer.v1;
+
+/**
+ * @author Haojie
+ * 披着面向对象外衣的面向过程
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		boolean cry = false;
+		
+		while(!cry) {
+			//进行处理
+		}
+
+	}
+
+}
